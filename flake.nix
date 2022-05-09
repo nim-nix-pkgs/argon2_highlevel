268f0f56e5d3612c843d65afa1b29fc86126fc65
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:08 -0300

    chore: re index argon2_highlevel

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."argon2_highlevel-master".dir   = "master";
-  inputs."argon2_highlevel-master".owner = "nim-nix-pkgs";
-  inputs."argon2_highlevel-master".ref   = "master";
-  inputs."argon2_highlevel-master".repo  = "argon2_highlevel";
-  inputs."argon2_highlevel-master".type  = "github";
-  inputs."argon2_highlevel-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."argon2_highlevel-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."argon2_highlevel-1_0_0".dir   = "1_0_0";
-  inputs."argon2_highlevel-1_0_0".owner = "nim-nix-pkgs";
-  inputs."argon2_highlevel-1_0_0".ref   = "master";
-  inputs."argon2_highlevel-1_0_0".repo  = "argon2_highlevel";
-  inputs."argon2_highlevel-1_0_0".type  = "github";
-  inputs."argon2_highlevel-1_0_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."argon2_highlevel-1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
